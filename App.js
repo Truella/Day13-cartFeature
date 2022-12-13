19e0a96c1023844a57b10e4ded18e6c59f39ec93
Truella <101476258+Truella@users.noreply.github.com>
2022-12-13 22:42:37 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,114 @@
+const toggleButton = document.querySelector(".item-two");
+const ul = document.getElementById("listParent")
+toggleButton.addEventListener("click", function () {
+				ul.classList.toggle("changeVisibility");		
+})
+        //End of header. 
+let figureArray = document.querySelectorAll("figure")
+const buttons = document.querySelectorAll("input");
+buttons.forEach(function (button) {
+				button.addEventListener("click",function () {
+								if(button.classList.contains("allBtn")) {
+												figureArray.forEach(function (figure) {
+																figure.style.display = "flex"
+												})
+												//
+								}
+								//ifstat
+								if(button.classList.contains("cakeBtn")) {
+												figureArray.forEach(function (figure) {
+																figure.style.display = "flex"
+												})
+												figureArray.forEach(function (figure) {
+																if(!(figure.classList.contains("cakes"))) {
+																				figure.style.display = "none";
+																}
+												})
+												//
+								}
+								//ifstate
+								if(button.classList.contains("doughnutBtn")) {
+												figureArray.forEach(function (figure) {
+																figure.style.display = "flex"
+												})
+												figureArray.forEach(function (figure) {
+																if(!(figure.classList.contains("doughnut"))) {
+																				figure.style.display = "none";
+																}
+												})
+												//
+								}
+								//ifstate
+								if(button.classList.contains("iceCreamBtn")) {
+												figureArray.forEach(function (figure) {
+																figure.style.display = "flex"
+												})
+												figureArray.forEach(function (figure) {
+																if(!(figure.classList.contains("iceCream"))) {
+																				figure.style.display = "none";
+																}
+												})
+												//
+								}
+								//ifstate
+								if(button.classList.contains("sweetBtn")) {
+												figureArray.forEach(function (figure) {
+																figure.style.display = "flex"
+												})
+												figureArray.forEach(function (figure) {
+																if(!(figure.classList.contains("sweet"))) {
+																				figure.style.display = "none";
+																}
+												})
+												//
+								}
+								//ifstate
+				})
+				//eventlistener
+})
+/// filter feature
+const searchFilter = document.getElementById("searchFilter");
+searchFilter.addEventListener("input", function () {
+				for(var i = 0; i < figureArray.length; i++){
+								if(figureArray[i].innerText.toUpperCase().includes(searchFilter.value.toUpperCase())) {
+												figureArray[i].classList.remove("is-hidden")
+								}else {
+												figureArray[i].classList.add("is-hidden")
+								}
+				}
+})
+/// Modal pop-up
+const imageArray = document.querySelectorAll(".open-modal");
+const hideModalBtn = document.getElementById("hideModal");
+const overlay = document.getElementById("Overlay")
+const modal = document.getElementById("Modal");
+						//AddEventListenerToBtn
+hideModalBtn.addEventListener("click", function () {
+		 	modal.classList.add("hidden")
+		 	overlay.classList.add("hidden")
+})
+
+//CART FEATURE
+const cartBox = document.querySelectorAll(".fa-cart-shopping");
+cartBox.forEach(function (cart) {
+				cart.classList.add("display-cart-bottom")
+				parent = cart.parentNode
+				parent.classList.add("hidden")
+				cart.addEventListener("click", function () {
+												cart.style.color = "#F17897";
+												alert("Item has been added to cart 🛒 ")
+								})
+				
+})
+imageArray.forEach(function (image) {
+				image.addEventListener("click", function () {
+								modal.classList.remove("hidden");
+								overlay.classList.remove("hidden");
+								cartBox.forEach(function (cart) {
+				parent = cart.parentNode
+				parent.classList.remove("hidden")
+				})
+								Url= image.src;
+								modal.style.backgroundImage= `url(${Url})`
+				}) //eventList
+})
